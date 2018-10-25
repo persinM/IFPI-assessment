a46f55bfcfa5b9457ce11a97e8ef21738899d84d
persinM <mchl.persin@gmail.com>
2018-10-25 11:21:37 +0100

    added documentation for task2

--- Task2Amend.py
+++ Task2Amend.py
@@ -1,6 +1,11 @@
 from websites.resources.data import WEBSITES
 
 def amend_data(sites):
+    """
+    param sites: list of website data.
+
+    Amends the data so its domains begin with 'www.'
+    """
     for data_item in sites:
         if data_item['domain'][:3] != "www.":
             data_item['domain'] = "www." + data_item['domain']
