a12aa68416882cc8c7ecf9f11c2400357d093c71
persinM <mchl.persin@gmail.com>
2018-10-25 11:38:25 +0100

    added documentation to task4

--- Task4Calculation.py
+++ Task4Calculation.py
@@ -1,6 +1,11 @@
 from websites.resources.data import WEBSITES
 
 def data_calculation(sites):
+    """
+    param sites: list of website data.
+
+    Returns the total of all site values.
+    """
     total = 0
     for data_item in sites:
         total += data_item['value']
