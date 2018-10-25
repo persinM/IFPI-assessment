abd5a9389614beb9b123abe30d0c1f7d199210a6
persinM <mchl.persin@gmail.com>
2018-10-25 11:31:03 +0100

    added documentation to task3

--- Task3Cleanse.py
+++ Task3Cleanse.py
@@ -1,6 +1,12 @@
 from websites.resources.data import WEBSITES
 
 def cleanse_data(sites):
+    """
+    param sites: list of website data
+
+    Checks and updates sites so that 'https' urls are set to
+    secure = True and 'http' urls are set to secure = False
+    """
     for data_item in sites:
         if data_item['url'][4] == 's':
             data_item['secure'] = True
