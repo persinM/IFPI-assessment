4c45aa408da551fbe513eb5444cba3570c0827b7
persinM <mchl.persin@gmail.com>
2018-10-25 11:10:16 +0100

    Added documentation to task1

--- Task1Find.py
+++ Task1Find.py
@@ -1,9 +1,17 @@
 from websites.resources.data import WEBSITES
 
-def find_data(sites, key):
+
+def find_data(sites, value):
+    """
+    param sites: list of website data;
+    param value: integer;
+
+    Returns a list of all the data in sites that has a value
+    greater than or equal to the inputted value.
+    """
     new_data = []
     for data_item in sites:
-        if data_item['value'] >= key:
+        if data_item['value'] >= value:
             new_data.append(data_item)
     return new_data
 
